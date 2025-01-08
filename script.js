894af3f5126149e58f2e51107133292f8759e39f
FarazAyaz <ayazf313@gmail.com>
2025-01-08 22:24:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('i am faraz');
+var a = 10;
+var b = 20;
+var c = "faraz";
+console.log(a + b + c)
+console.log(typeof a, typeof b, typeof c)
+
+let x = 10;
+let y = "FARAZ";
+const z = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, q, r)
+console.log(typeof x, typeof y, typeof z, typeof q, typeof r)
+
+let f = {
+    name: "faraz",
+    age: 20,
+}
+console.log(f)
+f.salary = "2 lakhs";
+console.log(f)
+
+f.game = ["cricket", "football", "hockey"]; 
