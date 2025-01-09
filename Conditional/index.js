498fac605c14fc4126be323f87238a3ea6a6d1fb
FarazAyaz <ayazf313@gmail.com>
2025-01-09 18:58:14 +0500

    node js in terminal

---
+++ Conditional/index.js
@@ -0,0 +1,16 @@
+
+// based on a condition we need to modify a variable
+let age = 17;
+let grace = 2;
+console.log(age + grace);
+console.log(age - grace);
+console.log(age / grace);
+console.log(age ** grace);
+
+if ((age+grace)>18)
+  {console.log("you are eligible for the vote")
+
+}
+else{
+  console.log("you are not eligible for the vote")
+}
