25510180c38a44480e05b9c1bfad3e76edcdf1f5
FarazAyaz <ayazf313@gmail.com>
2025-01-10 23:53:41 +0500

    loops and for of

--- Conditional/index.js
+++ Conditional/index.js
@@ -2,28 +2,40 @@
 // based on a condition we need to modify a variable
 // 
 
-let age = 10;
-if (age != 10) {
-  console.log("you can swim")
+// let age = 10;
+// if (age != 10) {
+//   console.log("you can swim")
 
-}
-else if(age==10){
-  console.log("Are You Kidding")
-}
+// }
+// else if(age==10){
+//   console.log("Are You Kidding")
+// }
 
-else{
+// else{
 
-  console.log("you cant swim")
-}
-let a = 1;
+//   console.log("you cant swim")
+// }
+  // loops
+// let a = 1;
 
 // for (let i = 0; i < 100; i++) {
 //   console.log(a + i);
   
   
 // }
-for (let f = 5; f < 100; f++) {
-  console.log(a + f);
+// for (let f = 5; f < 100; f++) {
+//   console.log(a + f);
   
+  
+// }
+//
+
+//  portion while
+
+
+let i = 2;
+while (i < 100) {
+  console.log(i);
+  i++;
   
 }
