cbc13894fc48a7ca76fc3aa1ebafc19f6e1d10c4
FarazAyaz <ayazf313@gmail.com>
2025-01-14 23:43:34 +0500

    arithematic operators comparison operators

--- Conditional/index.js
+++ Conditional/index.js
@@ -20,6 +20,7 @@
 
 // for (let i = 0; i < 100; i++) {
 //   console.log(a + i);
+ 
   
   
 // }
@@ -40,17 +41,58 @@
   
 // }
 // practice set
-var f = "FARAZ";
-var a = 10;
-console.log(f + a);
-console.log(typeof (f + a));
+// var f = "FARAZ";
+// var a = 10;
+// console.log(f + a);
+// console.log(typeof (f + a));
 
-const dict = {
-  appreciate: "To Praise Someone",
-  vigilant: "To be Alert",
-  respect: "To Honour Someone",
-  honest: "To be Truthful",
+// const dict = {
+//   appreciate: "To Praise Someone",
+//   vigilant: "To be Alert",
+//   respect: "To Honour Someone",
+//   honest: "To be Truthful",
 
-}
-console.log(dict.appreciate, dict.vigilant, dict.respect, dict.honest);
+// }
+// console.log(dict.appreciate, dict.vigilant, dict.respect, dict.honest);
 
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log("++a = ", ++a);
+// console.log(--a);
+// let a = 1;
+
+// for (let i = 0; i < 2; i++) {
+//         console.log(a + i);
+//       }
+
+let assignment = 10;
+assignment += 15;
+console.log(assignment);
+console.log(assignment -= 15);
+console.log(assignment *= 15);
+console.log(assignment /= 15);
+
+// comparison operators
+let faraz = 10;
+let adnan = 20;
+console.log(faraz == adnan);
+console.log(faraz != adnan);
+console.log(faraz > adnan);
+console.log(faraz < adnan);
+console.log(faraz >= adnan);
+console.log(faraz <= adnan);
+console.log(faraz === adnan);
+console.log(faraz !== adnan);
+
+
+ 
+
+  
+  
