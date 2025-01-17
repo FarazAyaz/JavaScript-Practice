2d99c90d1dee2aa6b8508952e375518ce21442fe
FarazAyaz <ayazf313@gmail.com>
2025-01-17 23:54:34 +0500

    chap 2 practice set

--- Conditional/index.js
+++ Conditional/index.js
@@ -96,18 +96,56 @@
 // a = Number.parseInt(a);
       
 
-let fruit= "Mango"
-switch(fruit){
-    case  "Orange" :
-        console.log("oranges are my fav")
-    break
-    case  "Mango" :   
-        console.log("Mangoes are my fav")
-    break
-    default :
-    console.log("Sorry")
-    
+// 
+
+practice set chap 2 
+question 1
+let age = 15
+if(age>10 && age<20){
+    console.log("Your Age lies Between 10 And 20")
+}
+else{
+    console.log("Your age doesnt between 10 and 20")
 }
 
+let age = (15)
+switch (age){
+    case 12:
+    console.log("Your age is 12")
+    break
+    case 13:
+    console.log("Your age is 13")
+    break
+    case 14:
+    console.log("Your age is 14")
+    break
+    case 15:
+    console.log("Your age is 15")
+    break
+    default:
+        console.log("your are not special")
+    
+
+}
+question 3
+let num = 12
+if(num%2==0 && num%3==0){
+    console.log('the number is divisible by two and three')
+}
+else(
+    console.log('the number is not divisible by two and three')
+)
+
+question no 4
+
+let num = 45
+if(num%2==0 || num%3==0){
+    console.log('the number is divisible by two and three')
+}
+else(
+    console.log('the number is not divisible by two and three')
+)
   
-  
+let age = 15
+let a = age>18?"you can drive":"you cannot drive"
+console.log(a)
--- Conditional/tempCodeRunnerFile.js
+++ Conditional/tempCodeRunnerFile.js
@@ -1,1 +1,39 @@
-const prompt=require("prompt-sync")();
+practice set chap 2 
+question 1
+let age = 15
+if(age>10 && age<20){
+    console.log("Your Age lies Between 10 And 20")
+}
+else{
+    console.log("Your age doesnt between 10 and 20")
+}
+
+let age = (15)
+switch (age){
+    case 12:
+    console.log("Your age is 12")
+    break
+    case 13:
+    console.log("Your age is 13")
+    break
+    case 14:
+    console.log("Your age is 14")
+    break
+    case 15:
+    console.log("Your age is 15")
+    break
+    default:
+        console.log("your are not special")
+    
+
+}
+question 3
+let num = 12
+if(num%2==0 && num%3==0){
+    console.log('the number is divisible by two and three')
+}
+else(
+    console.log('the number is not divisible by two and three')
+)
+
+question no 4
