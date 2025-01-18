9cae0d94602dfc450cff876f988a390e5fa112fb
FarazAyaz <ayazf313@gmail.com>
2025-01-18 23:53:35 +0500

    loops

--- Conditional/index.js
+++ Conditional/index.js
@@ -98,54 +98,99 @@
 
 // 
 
-practice set chap 2 
-question 1
-let age = 15
-if(age>10 && age<20){
-    console.log("Your Age lies Between 10 And 20")
-}
-else{
-    console.log("Your age doesnt between 10 and 20")
-}
+//                                        practice set chap 2 
 
-let age = (15)
-switch (age){
-    case 12:
-    console.log("Your age is 12")
-    break
-    case 13:
-    console.log("Your age is 13")
-    break
-    case 14:
-    console.log("Your age is 14")
-    break
-    case 15:
-    console.log("Your age is 15")
-    break
-    default:
-        console.log("your are not special")
+//                                         question 1
+
+// let age = 15
+// if(age>10 && age<20){
+//     console.log("Your Age lies Between 10 And 20")
+// }
+// else{
+//     console.log("Your age doesnt between 10 and 20")
+// }
+//                                         question 2
+
+// let age = (15)
+// switch (age){
+//     case 12:
+//     console.log("Your age is 12")
+//     break
+//     case 13:
+//     console.log("Your age is 13")
+//     break
+//     case 14:
+//     console.log("Your age is 14")
+//     break
+//     case 15:
+//     console.log("Your age is 15")
+//     break
+//     default:
+//         console.log("your are not special")
     
 
-}
-question 3
-let num = 12
-if(num%2==0 && num%3==0){
-    console.log('the number is divisible by two and three')
-}
-else(
-    console.log('the number is not divisible by two and three')
-)
+// }
+//                                           question 3
 
-question no 4
+// let num = 12
+// if(num%2==0 && num%3==0){
+//     console.log('the number is divisible by two and three')
+// }
+// else(
+//     console.log('the number is not divisible by two and three')
+// )
 
-let num = 45
-if(num%2==0 || num%3==0){
-    console.log('the number is divisible by two and three')
-}
-else(
-    console.log('the number is not divisible by two and three')
-)
+//                                       question no 4
+
+// let num = 45
+// if(num%2==0 || num%3==0){
+//     console.log('the number is divisible by two and three')
+// }
+// else(
+//     console.log('the number is not divisible by two and three')
+// )
+
+// question 5
   
-let age = 15
-let a = age>18?"you can drive":"you cannot drive"
-console.log(a)
+// let age = 15
+// let a = age>18?"you can drive":"you cannot drive"
+// console.log(a)
+
+//                                chap 3
+
+//   .....................for loop.....................
+
+// let factorial = 1;
+// let n = 5
+// for (let i = 0; i < n; i++) {
+//     factorial *= (i +1);
+    
+// }
+// console.log(
+//     'factorial of first' + n + "natural number is" + factorial 
+// )
+
+
+// let sum = 0;
+// let n = 10;
+// for (let i = 0; i < n; i++) {
+//     sum += (i + 1);
+    
+// }
+// console.log(
+//     sum
+// )
+
+let obj = {
+  faraz: 100,
+  adnan: 99,
+  siffat: 98,
+  ahmed: 90,
+  khan: 81
+};
+
+for (let a in obj) {
+  console.log("Marks of " + a + " are " + obj[a]);
+  console.log(); // Adds an empty line
+}
+
