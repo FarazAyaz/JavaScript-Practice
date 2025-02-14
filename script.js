b42344d44c09b2807d24e5012c5b06bb90dc7d28
FarazAyaz <ayazf313@gmail.com>
2025-02-14 23:35:53 +0500

    dom manipulation

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let b = document.body
-console.log("First Child of b is:" + b.firstChild )
-console.log("First Child of b is:" + b.firstElementChild )
-
-let ctitles = document.getElementsByClassName()
+// let b = document.body
+// console.log("First Child of b is:" + b.firstChild )
+// console.log("First Child of b is:" + b.firstElementChild )
 
 
+//   let ctitles = document.getElementById("myId");
+//   ctitles.style.color = "green"; // Now it will work
+
+//   document.querySelector(".title").style.color = "red";
+  
+// window.onload = function () {
+// let my_paragraph = document.getElementsByClassName(".container");
+
+// my_paragraph.style.color = "green";
+// }
