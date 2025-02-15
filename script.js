d6b7ea57febec65b55149fa4b839abb727750fcc
FarazAyaz <ayazf313@gmail.com>
2025-02-15 19:10:37 +0500

    commit

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 // console.log("First Child of b is:" + b.firstChild )
 // console.log("First Child of b is:" + b.firstElementChild )
 
-
 //   let ctitles = document.getElementById("myId");
 //   ctitles.style.color = "green"; // Now it will work
 
 //   document.querySelector(".title").style.color = "red";
-  
+
 // window.onload = function () {
 // let my_paragraph = document.getElementsByClassName(".container");
 
 // my_paragraph.style.color = "green";
 // }
+
+// const title = document.getElementById("title");
+// title.style.backgroundColor = "aqua"
