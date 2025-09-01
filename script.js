cd6892ee18c8a71bc71d003d60b6517314f22886
Faraz Ayaz <farazayazkhan1@gmail.com>
2025-09-01 23:28:45 +0500

    COMMIT

--- script.js
+++ script.js
@@ -16,10 +16,11 @@
 // const title = document.getElementById("title");
 // title.style.backgroundColor = "aqua"
 
-const parent  = document.querySelector(".parent") 
-console.log(parent);
-console.log(parent.children);
-for (let i = 0; i < parent.children.length; i++) {
-     console.log(parent.children[i].innerHTML);
+// const parent  = document.querySelector(".parent") 
+// console.log(parent);
+// console.log(parent.children);
+// for (let i = 0; i < parent.children.length; i++) {
+//      console.log(parent.children[i].innerHTML);
     
-}
+// }
+
